ef3d8fdef8a8ac0d4425f41e01018b2490672221
SkyJourney <sky.jiang.s.z@gmail.com>
2020-06-12 11:17:52 +0800

    Day 9 Config

---
+++ eurekaserverconfig/src/test/java/com/youyue/eurekaserverconfig/EurekaserverconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youyue.eurekaserverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
